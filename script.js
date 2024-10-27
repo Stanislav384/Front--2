65eebcf1d556b35f322eed51ebd1ef84ba783c9b
Whisper25 <whisper252552@gmail.com>
2024-10-27 21:34:15 +0100

    4

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 // ВАШІ СКРИПТИ
+const btn = document.querySelector('.btn');
+btn.addEventListener('click', () => {
+    btn.style.backgroundColor = '#ffac09';
+    btn.style.boxShadow = '0 0 40px #ffac09';
+    btn.style.transition = '.5s ease';
+});
+
